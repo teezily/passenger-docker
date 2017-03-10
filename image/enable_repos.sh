5a7f9ef31e46c230577064bc88af4998efc40620
zacviandier <zacviandier@gmail.com>
2017-03-10 14:08:04 +0800

    Use phusion/baseimage:0.9.19 for base image to get ubuntu 16.04

--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -5,8 +5,6 @@ set -x
 
 ## Brightbox Ruby 1.9.3, 2.0, 2.1 and 2.2
 echo deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main > /etc/apt/sources.list.d/brightbox.list
-echo deb http://ppa.launchpad.net/nginx/stable/ubuntu trusty main > /etc/apt/sources.list.d/nginx-stable.list
-
 
 # The recv-keys part takes a bit of time, so it's faster to receive multiple keys at once.
 #
